4b1b66dd92f0a38a2771b9be6b161a88627355e1
ProjectCitadelOne <41846392+ProjectCitadelOne@users.noreply.github.com>
2018-12-15 10:20:51 -0500

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+# List the first 100 numbers in the fibonacci sequence.
+
+a = 0
+b = 1
+count = 0
+
+# Print first two numbers in the sequence.
+print(a)
+print(b)
+# Find and print the next 98 numbers in the sequence.
+while count < 98:
+    sum = a + b
+    print(sum)
+    a = b
+    b = sum
+    count += 1
